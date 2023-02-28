61b56ca72c5f0ee4768e3a68f76ec0fa356b1807
Nikolayz <zans55@yandex.ru>
2023-02-28 17:23:12 +0300

    82

---
+++ 82/Program.cs
@@ -0,0 +1,11 @@
+﻿/* C клавиатуры вводится строка разделенная точкой. 
+Подсчитать количество символов до точки*/
+
+int k=0;
+string s=Console.ReadLine();
+char[]s1=s.ToCharArray();
+for(int i=0;i<s1.Length;i++)
+if (s[k]!='.')
+k++;
+Console.WriteLine($"{k}");
+
