f374ceef6bdcfc34a9e96ade061b3c0bd1322252
Nikolayz <zans55@yandex.ru>
2023-02-28 18:30:09 +0300

    86

---
+++ 86/Program.cs
@@ -0,0 +1,9 @@
+﻿/*Подсчитать сколько раз определенный символ (например 'a') 
+встречается в строке.*/
+string a=Console.ReadLine();
+int k=0;
+for (int i=0;i<a.Length;i++) 
+{
+    if (a[i]=='a') k++;
+}
+Console.WriteLine(k);
