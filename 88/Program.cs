9e75f1aacdedb8716207c5a005b83beb3d8b9d44
Nikolayz <zans55@yandex.ru>
2023-02-28 18:47:57 +0300

    88

---
+++ 88/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Заменить все вхождения подстроки w в строке st на строку v. 
+Вывести на экран первоначальную строку и конечную строку*/
+
+string s=Console.ReadLine();
+char[] s1=s.ToCharArray();
+for (int i=0;i<s.Length;i++)
+{
+    if (s[i]=='w')
+    s1[i]='v';
+    
+}
+Console.WriteLine(s);
+Console.WriteLine(s1);
